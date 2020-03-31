feacdd3b26c817b7d13745ea2d4f03e7f4a76c08
bbedevian <brettbedevian@gmail.com>
2020-03-31 12:13:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    post '/piglatinize' do
+        pl = PigLatinizer.new
+        @piglatin = pl.piglatinize(params[:user_phrase])
+        erb :results
+      end
+
+    get '/' do 
+        erb :user_input
+    end
 end
